cef5decce7c5ef3b92cdcfbaf91115b17c224946
Jacob <jschwel1@binghamton.edu>
2016-05-04 14:50:10 -0400

    Added comments to the #defines in maze.h

--- maze.h
+++ maze.h
@@ -13,15 +13,15 @@
 #define BLACK 0xFF
 #define WHITE 0x00
 
-#define ON_LINE 			((WHITE & 0xF0) | (BLACK & 0x0F))
-#define RIGHT_DRIFT 		((WHITE & 0xE0) | (BLACK & 0x1F))
-#define LEFT_DRIFT 			((WHITE & 0xF8) | (BLACK & 0x03))
-#define OFF_TO_THE_RIGHT 	((WHITE & 0xC0) | (BLACK & 0x3F))
-#define OFF_TO_THE_LEFT 	((WHITE & 0xFC) | (BLACK & 0x03))
-#define ALMOST_ALL_BLACK 	((WHITE & 0x80) | (BLACK & 0x7F))
-#define ALMOST_ALL_WHITE 	((WHITE & 0xF7) | (BLACK & 0x01))
-#define ALL_WHITE 			(WHITE)
-#define ALL_BLACK 			(BLACK)
+#define ON_LINE 			((WHITE & 0xF0) | (BLACK & 0x0F)) // WWWW BBBB
+#define RIGHT_DRIFT 		((WHITE & 0xE0) | (BLACK & 0x1F)) // WWWB BBBB
+#define LEFT_DRIFT 			((WHITE & 0xF8) | (BLACK & 0x03)) // WWWW WBBB
+#define OFF_TO_THE_RIGHT 	((WHITE & 0xC0) | (BLACK & 0x3F)) // WWBB BBBB
+#define OFF_TO_THE_LEFT 	((WHITE & 0xFC) | (BLACK & 0x03)) // WWWW WWBB
+#define ALMOST_ALL_BLACK 	((WHITE & 0x80) | (BLACK & 0x7F)) // WBBB BBBB
+#define ALMOST_ALL_WHITE 	((WHITE & 0xF7) | (BLACK & 0x01)) // WWWW WWWB
+#define ALL_WHITE 			(WHITE)							  // WWWW WWWW
+#define ALL_BLACK 			(BLACK)							  // BBBB BBBB
 
 // navigateMaze() utilizes the functions in this file, motor.h, 
 // and qtr_driver.h to drive through the maze using a right-hand search.
